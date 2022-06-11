53a77d1ceddad2edb6060034cb47b638861be76b
Godnessy <godnessy@gmail.com>
2022-06-11 19:47:02 +0200

    migrated the app from another repo, added minor bug fixes and some UI color changes)

---
+++ script.js
@@ -0,0 +1,214 @@
+"use strict";
+/* TODO:
+3. find way to change calc to accept numbers (get input from user*10 +input *10 etc etc and then /10 when input goes to operator)
+4. make all outputs intergers and not strings.
+7. Make the Ui nicer - Maybe use tailwindCSS for the exp. 
+*/
+let arg1 = "";
+let arg2 = "";
+let result = "";
+let tempResult = "";
+let operand = null;
+let historyBox = new Map();
+const showUi = (ui, value) => {
+  ui.innerHTML = value;
+};
+
+//UI change variables
+const arg1_ui = document.getElementById("arg1");
+const arg2_ui = document.getElementById("arg2");
+const result_ui = document.getElementById("result");
+const operand_ui = document.getElementById("operand");
+const history_ui = document.getElementById("historyResult");
+const historyCalc_ui = document.getElementById("historyCalc");
+
+// History display/hide
+
+document.getElementById("histDisp").onclick = showHide;
+
+function showHide() {
+  document.getElementById("historyContainer").style.display == "none"
+    ? (document.getElementById("historyContainer").style.display = "grid")
+    : (document.getElementById("historyContainer").style.display = "none");
+}
+
+//keyboard input functionality
+const allowedString = "0 1 2 3 4 5 6 7 8 9 Enter Backspace . + - * /";
+
+window.addEventListener("keyup", (e) => {
+  const key = e.key;
+  if (!allowedString.split(" ").includes(key)) {
+    return;
+  }
+  if (Number.isInteger(Number(key))) {
+    return calc(key);
+  }
+  switch (key) {
+    case "+":
+    case "-":
+    case "*":
+      return operator(key);
+    case "/":
+      return operator("÷");
+    case "Enter":
+      return showResult();
+    case "Backspace":
+      console.log("reset pressed");
+      return reset();
+    case ".":
+    case ",":
+      return calc(key);
+  }
+});
+
+//Number button selector
+document.querySelectorAll(".numberBtn").forEach((a) => {
+  a.addEventListener("click", (a) => {
+    calc(Number(a.target.textContent));
+  });
+});
+
+//Operator button selector
+document.querySelectorAll(".operandBtn").forEach((a) => {
+  a.addEventListener("click", (a) => {
+    operator(a.target.textContent);
+  });
+});
+
+//Plus minus functionality
+let plusMinus = () => {
+  if (operand == null) {
+    console.log("lvl1");
+    String(arg1)[0] == "-"
+      ? (arg1 = String(arg1).substring(1))
+      : (arg1 = `-${arg1}`);
+  } else {
+    console.log("lvl2");
+    arg2[0] === "-" ? (arg2 = arg2.substring(1)) : (arg2 = `-${arg2}`);
+    showUi(arg2_ui, "");
+  }
+  showUi(result_ui, "");
+  showUi(arg1_ui, arg1);
+  showUi(arg2_ui, arg2);
+};
+document.getElementById("plusMinus").onclick = plusMinus;
+
+//fractional numbers functionality
+let fraction = () => {
+  showUi(result_ui, "");
+  arg2 == "" ? (arg1 = `${arg1}.`) : (arg2 = `${arg2}.`);
+  showUi(arg1_ui, arg1);
+  showUi(arg2_ui, arg2);
+};
+
+document.getElementById("point").onclick = fraction;
+
+//Adding numbers to args
+function calc(num) {
+  if (operand === null) {
+    arg1 += num;
+    showUi(arg1_ui, arg1);
+  } else {
+    arg2 += num;
+    showUi(arg2_ui, arg2);
+  }
+}
+
+const operatorMap = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "÷": (a, b) => a / b,
+};
+
+//working the operator on the numbers set
+function operator(sign) {
+  operand = sign;
+  showUi(operand_ui, sign);
+  if (arg2 === "") return;
+  const func = operatorMap[sign];
+  result = func(Number(arg1), Number(arg2));
+  //Checking if the number is a float and if so returning only 3 numbers after the decimal point.
+  result = Number(result) % 1 ? result.toFixed(3) : result;
+  return result;
+}
+
+document.getElementById("reset").onclick = reset;
+function reset() {
+  // console.log("reset");
+  result = "";
+  arg1 = "";
+  arg2 = "";
+  operand = null;
+  showUi(arg1_ui, arg1);
+  showUi(result_ui, "");
+  showUi(arg2_ui, arg2);
+  showUi(operand_ui, null);
+}
+
+//record the args, operator and result to be saved into history calc object
+function recordHistory(arg1, operator, arg2, result) {
+  let calc = String(`${arg1} ${operator} ${arg2}`);
+  historyBox.set(result, calc);
+}
+
+//Insert history into 2 lists to be picked later by clicking on them
+function showHistory(historyBox) {
+  showUi(history_ui, "");
+  showUi(historyCalc_ui, "");
+  for (const [key, value] of historyBox) {
+    const newValue = `<li class ="data" id="histResult">= ${String(key)}</li>`;
+    const newCalc = `<li class ="data" id="histCalc">${String(value)}</li>`;
+    history_ui.insertAdjacentHTML("afterbegin", newValue);
+    historyCalc_ui.insertAdjacentHTML("afterbegin", newCalc);
+  }
+}
+
+//Adding elements from history back to calculation
+document.querySelectorAll(".dataInputs").forEach((a) => {
+  a.addEventListener("click", (a) => {
+    let innerHtml = a.target.innerHTML;
+
+    if (a.target.id == "histCalc") {
+      reset();
+      let futureCalc = innerHtml.split(" ");
+      calc(futureCalc[0]);
+      operator(futureCalc[1]);
+      calc(futureCalc[2]);
+    } else if (operand == null) {
+      reset();
+      calc(innerHtml.substring(2));
+    } else if (arg2 == "") {
+      calc(innerHtml.substring(2));
+    }
+  });
+});
+
+document.getElementById("deleteHist").onclick = clearHistory;
+function clearHistory() {
+  historyBox.clear();
+  showUi(history_ui, "");
+  showUi(historyCalc_ui, "");
+}
+
+//UI functions
+document.getElementById("equals").onclick = showResult;
+
+function showResult() {
+  if (operand == null) {
+    return;
+  } else if (arg2 == "") {
+    return;
+  } else {
+    operator(operand);
+    recordHistory(arg1, operand, arg2, result);
+    showHistory(historyBox);
+    showUi(arg1_ui, "");
+    showUi(arg2_ui, "");
+    showUi(operand_ui, "");
+    showUi(result_ui, result);
+    operand = null;
+    arg1 = result;
+    arg2 = "";
+  }
+}
